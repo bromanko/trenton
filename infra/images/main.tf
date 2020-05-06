25b33817a9e5d4093bb9b6991718229c7e2f41f9
Brian Romanko <brian@bestow.co>
2020-05-06 08:43:12 -0500

    Remove AppEngine config

--- infra/images/main.tf
+++ infra/images/main.tf
@@ -15,7 +15,6 @@ provider "google" {
 }
 
 locals {
-  bucket_name = "artifacts.${var.project_id}.appspot.com"
   url         = data.google_container_registry_repository.main.repository_url
 }
 
@@ -52,39 +51,6 @@ resource "google_service_account" "ro" {
 
 data "google_container_registry_repository" "main" {
   project = var.project_id
-}
-
-
-# google_storage_bucket_iam_binding resources are authoritative for their respective roles
-# if an entity isn't in its list of members their access will be revoked
-
-# storage.objectAdmin provides complete access to objects in a bucket, but no access to the bucket itself
-# this is one of two roles required to grant docker push access
-resource "google_storage_bucket_iam_member" "object_admin" {
-  bucket     = local.bucket_name
-  role       = "roles/storage.objectAdmin"
-  member     = "serviceAccount:${google_service_account.rw.email}"
-  depends_on = [google_project_service.containerregistry]
-}
-
-# docker push also requires storage.buckets.get in order to work properly
-# storage.legacyBucketReader is the least permissive role that provides this
-resource "google_storage_bucket_iam_member" "legacy_bucket_reader" {
-  bucket     = local.bucket_name
-  role       = "roles/storage.legacyBucketReader"
-  member     = "serviceAccount:${google_service_account.rw.email}"
-  depends_on = [google_project_service.containerregistry]
-}
-
-# google_storage_bucket_iam_member resources are not authoritative for their roles
-# additional members can be configured outside this module
-
-# storage.objectViewer enables an entity to docker pull from the repository
-resource "google_storage_bucket_iam_member" "object_viewer" {
-  bucket     = local.bucket_name
-  role       = "roles/storage.objectViewer"
-  member     = "serviceAccount:${google_service_account.ro.email}"
-  depends_on = [google_project_service.containerregistry]
 }
 
 resource "google_service_account_key" "ro_key" {
