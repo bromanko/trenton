5f16fecfb543ff0a650f548811b909bec8bfa8c5
Brian Romanko <hello@bromanko.com>
2020-08-12 15:51:55 -0500

    Remove debugging output

--- core/scripts/itest-entrypoint.sh
+++ core/scripts/itest-entrypoint.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 script_path="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "${script_path}"
 
-set -x
-TEST_SERVER_HOST=server:5000
 ./dev-entrypoint.sh \
   -wait "http://${TEST_SERVER_HOST}" \
   -- dotnet run --project test/Trenton.Webhooks.ITests/Trenton.Webhooks.ITests.fsproj
