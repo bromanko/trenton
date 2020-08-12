5b271834918eea00d98da4dd0e4ca7660b08ffa9
Brian Romanko <hello@bromanko.com>
2020-08-12 17:01:47 -0500

    Add a test for retrieving values from Firestore

--- core/src/Trenton.Webhooks/Firestore.fs
+++ core/src/Trenton.Webhooks/Firestore.fs
@@ -6,6 +6,9 @@ open Trenton.Health.FitbitClient
 
 module Firestore =
     module Fitbit =
+        [<Literal>]
+        let AccessTokenCollectionName = "fitbit-access-tokens"
+
         [<FirestoreData; CLIMutable>]
         type AccessToken =
             { [<FirestoreProperty>]
@@ -17,14 +20,25 @@ module Firestore =
 
         let private parseAccessToken (token: AccessTokenDto) (now: DateTime) =
             { AccessToken.AccessToken = token.AccessToken
-              ExpiresAt = now.ToUniversalTime().AddSeconds <| (float token.ExpiresInSeconds)
+              ExpiresAt =
+                  now.ToUniversalTime().AddSeconds
+                  <| (float token.ExpiresInSeconds)
               RefreshToken = token.RefreshToken }
 
         let upsertAccessToken (db: FirestoreDb) getNow token tokenState =
             let docRef =
-                db.Collection("fitbit-access-tokens")
+                db.Collection(AccessTokenCollectionName)
                   .Document(tokenState.UserId)
             getNow ()
             |> parseAccessToken token
             |> docRef.SetAsync
             |> Async.AwaitTask
+
+        let tryGetAccessToken (db: FirestoreDb) userId =
+            async {
+                let docRef =
+                    db.Collection(AccessTokenCollectionName).Document(userId)
+                match! docRef.GetSnapshotAsync() |> Async.AwaitTask with
+                | s when s.Exists -> return s.ConvertTo<AccessToken>() |> Some
+                | _ -> return None
+            }
--- core/test/Trenton.Webhooks.ITests/FirestoreTests.fs
+++ core/test/Trenton.Webhooks.ITests/FirestoreTests.fs
@@ -4,13 +4,21 @@ open Config
 open Expecto
 open Google.Api.Gax
 open Google.Cloud.Firestore
-open System
 open Grpc.Core
+open System
 open Trenton.Health.FitbitClient
 open Trenton.Webhooks.Firestore.Fitbit
 
 module FirestoreTests =
     let getNow () = DateTime.Now
+
+    let fakeAccessToken () =
+        { AccessToken = Guid.NewGuid().ToString()
+          ExpiresInSeconds = 200
+          RefreshToken = Guid.NewGuid().ToString() }
+
+    let fakeAccessTokenState () =
+        { UserId = Guid.NewGuid().ToString() }
 
     let buildDb () =
         let config = loadConfig ()
@@ -25,20 +33,26 @@ module FirestoreTests =
     let tests =
         testList "Firestore"
             [ testAsync "Stores access token" {
-                  let token =
-                      { AccessToken = "accessToken"
-                        ExpiresInSeconds = 200
-                        RefreshToken = "refreshToken" }
-
-                  let tokenState =
-                      { UserId = "userId" }
-
+                  let token = fakeAccessToken()
+                  let tokenState = fakeAccessTokenState()
                   let beforeSave = getNow ()
-
                   let! db = buildDb ()
+
                   let! result = upsertAccessToken db getNow token tokenState
 
                   Expect.isGreaterThan (result.UpdateTime.ToDateTime())
                       beforeSave
                       "UpdateTime was not set to after the save operation"
+              }
+              testAsync "Retrieves access token" {
+                  let token = fakeAccessToken()
+                  let tokenState = fakeAccessTokenState()
+                  let! db = buildDb ()
+
+                  do! upsertAccessToken db getNow token tokenState |> Async.Ignore
+                  match! tryGetAccessToken db tokenState.UserId with
+                  | None -> failtestf "Should have retrieved the access token"
+                  | Some a ->
+                      Expect.equal a.AccessToken token.AccessToken "Access token loaded did not match"
+                      Expect.equal a.RefreshToken token.RefreshToken "Refresh token loaded did not match"
               } ]
