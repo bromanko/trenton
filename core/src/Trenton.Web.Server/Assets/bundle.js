e4ccc53653054ee57249873e5d459868124e837d
Brian Romanko <hello@bromanko.com>
2020-08-25 14:45:26 -0500

    Getting alpine configured

--- core/src/Trenton.Web.Server/Assets/bundle.js
+++ core/src/Trenton.Web.Server/Assets/bundle.js
@@ -1,4 +1,9 @@
-import './css/main.css'
+import 'alpinejs'
 import Turbolinks from 'turbolinks'
+
+import './css/main.css'
+import data from './js/main.js'
+
+window.data = data
 
 Turbolinks.start()
