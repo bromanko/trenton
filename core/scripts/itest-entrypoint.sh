b609506fef744cb473178ee60fd1848e3186ea55
Brian Romanko <hello@bromanko.com>
2020-08-30 15:09:24 -0500

    Fix itest entrypoint path

--- core/scripts/itest-entrypoint.sh
+++ core/scripts/itest-entrypoint.sh
@@ -6,8 +6,8 @@ script_path="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "${script_path}"
 
 project=${1?Must provide the itest project name}
-project="$project/$project.fsproj"
-[[ -f "../test/$project" ]] || (echo "Could not find project $1" && exit 1)
+project="test/$project/$project.fsproj"
+[[ -f "../$project" ]] || (echo "Could not find project $1" && exit 1)
 
 ./dev-entrypoint.sh \
   -wait "http://${TEST_SERVER_HOST}" \
