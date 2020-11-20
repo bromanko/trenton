888cc0920fcdd8810ed1a978cec251dbe0084298
Brian Romanko <hello@bromanko.com>
2020-11-20 08:41:43 -0600

    Clean up parsing logic for CLI args

--- core/src/Trenton.Cli/Verbs/ExportFitbit.fs
+++ core/src/Trenton.Cli/Verbs/ExportFitbit.fs
@@ -1,10 +1,10 @@
 namespace Trenton.Cli.Verbs
 
 open Argu
-open FsToolkit.ErrorHandling
-open FsToolkit.ErrorHandling.Operator.Result
 open Trenton.Cli
+open Trenton.Cli.Verbs
 open Trenton.Common
+open FsToolkit.ErrorHandling.Operator.Result
 
 module ExportFitbit =
     type private ExportConfig =
@@ -12,33 +12,6 @@ module ExportFitbit =
           RefreshToken: NonEmptyString.T option
           StartDate: Date.T
           EndDate: Date.T option }
-
-    let private parseNes msg a =
-        match NonEmptyString.create a with
-        | None -> Result.Error <| ArgParseError msg
-        | Some s -> Ok s
-
-    let private parseOptionalNes msg a =
-        // ToDO fix this mess
-        match a with
-        | Some s ->
-            match parseNes msg s with
-            | Result.Error e -> Result.Error e
-            | Ok s -> Ok(Some s)
-        | None -> Ok None
-
-    let private parseDate msg a =
-        match Date.tryParse a with
-        | None -> Result.Error <| ArgParseError msg
-        | Some d -> Ok d
-
-    let private parseOptionalDate msg a =
-        match a with
-        | Some d ->
-            match parseDate msg d with
-            | Result.Error e -> Result.Error e
-            | Ok d -> Ok <| Some d
-        | None -> Ok None
 
     let private mkConfig accessToken refreshToken startDate endDate =
         { AccessToken = accessToken
@@ -49,13 +22,13 @@ module ExportFitbit =
     let private parseConfig (args: ParseResults<FitbitExportArgs>) =
         mkConfig
         <!> (parseNes "Access token must be specified."
-             <| args.GetResult <@ AccessToken @>)
+             <| args.GetResult AccessToken)
         <*> (parseOptionalNes "Refresh token must be a valid string."
-             <| args.TryGetResult <@ RefreshToken @>)
+             <| args.TryGetResult RefreshToken)
         <*> (parseDate "Start date must be a valid date."
-             <| args.GetResult <@ StartDate @>)
+             <| args.GetResult StartDate)
         <*> (parseOptionalDate "End date must be a valid date."
-             <| args.TryGetResult <@ EndDate @>)
+             <| args.TryGetResult EndDate)
 
     let exec (args: ParseResults<FitbitExportArgs>) =
         parseConfig args >>= (fun _ -> Ok())
---
+++ core/src/Trenton.Cli/Verbs/Parsing.fs
@@ -0,0 +1,25 @@
+namespace Trenton.Cli.Verbs
+
+open Trenton.Common
+open FsToolkit.ErrorHandling
+
+[<AutoOpen>]
+module Parsing =
+    let parse f msg a =
+        match f a with
+        | None -> Result.Error <| ArgParseError msg
+        | Some s -> Ok s
+
+    let parseNes = parse NonEmptyString.create
+
+    let parseDate = parse Date.tryParse
+
+    let parseOptionalNes msg a = Option.traverseResult (parseNes msg) a
+
+    let parseOptionalDate msg a =
+        match a with
+        | Some d ->
+            match parseDate msg d with
+            | Result.Error e -> Result.Error e
+            | Ok d -> Ok <| Some d
+        | None -> Ok None
