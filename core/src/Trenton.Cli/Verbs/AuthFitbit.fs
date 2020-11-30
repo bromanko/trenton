77bc887f61308ac0b1ff11b3778d2963da131d72
Brian Romanko <hello@bromanko.com>
2020-11-30 16:54:58 -0600

    Add Fitbit auth command
    This opens the browser to authenticate with Fitbit. It then redirects to
    localhost. Right now there is no server setup. That will come next.

--- core/src/Trenton.Cli/Verbs/AuthFitbit.fs
+++ core/src/Trenton.Cli/Verbs/AuthFitbit.fs
@@ -3,24 +3,53 @@ namespace Trenton.Cli.Verbs
 open Trenton.Common
 open Trenton.Cli
 open FsToolkit.ErrorHandling.Operator.Result
+open System.Diagnostics
+open System.Runtime.InteropServices
 
 module AuthFitbit =
-    type private AuthConfig =
-        { ClientId: NonEmptyString.T
-          ClientSecret: NonEmptyString.T }
+    [<Literal>]
+    let RedirectUri = "http://localhost:9032/fitbit/callback"
 
-    let private mkConfig clientId clientSecret =
-        { ClientId = clientId
-          ClientSecret = clientSecret }
+    let Scopes =
+        [| "activity"
+           "heartrate"
+           "location"
+           "nutrition"
+           "profile"
+           "settings"
+           "sleep"
+           "social"
+           "weight" |]
+
+    type private AuthConfig = { ClientId: NonEmptyString.T }
+
+    let private mkConfig clientId = { ClientId = clientId }
 
     let private parseCfg (cfg: AppConfig) =
         mkConfig
         <!> (parseNes "ClientId must be specified in app configuration file"
              <| cfg.Fitbit.ClientId)
-        <*> (parseNes
-                 "Client secret must be specified in app configuration file"
-             <| cfg.Fitbit.ClientSecret)
 
-    let exec cfg =
-        parseCfg cfg
-        >>= (fun _ -> Result.Error ConfigFileNotFound)
+    let private browser url =
+        if RuntimeInformation.IsOSPlatform OSPlatform.Windows then
+            ProcessStartInfo("cmd", sprintf "/c start %s" url)
+        elif RuntimeInformation.IsOSPlatform OSPlatform.OSX then
+            ProcessStartInfo("open", url)
+        else
+            ProcessStartInfo("xdg-open", url)
+
+    let private launchUrl cfg =
+        let url =
+            sprintf
+                "https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=%s&redirect_uri=%s&scope=%s&expires_in=2592000000"
+                (NonEmptyString.value cfg.ClientId)
+                RedirectUri
+                (String.concat "%20" Scopes)
+
+        try
+            browser url |> Process.Start |> ignore
+
+            Ok()
+        with ex -> ExecError.Exception ex |> Result.Error
+
+    let exec cfg = parseCfg cfg >>= launchUrl
